838ddcb72e5e932b0d1bb3fef80568aaaceb9668
ZekasDev <dannndourado@gmail.com>
2023-02-04 12:18:12 -0300

    Primeiros testes: OK

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.post("/users", (require, response) => {
-  const body = request.body;
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
 
-  return response.json({
-    evento: "Semana Omnistack 11.0",
-    aluno: "Toalhinhas",
-  });
+  return response.json(ongs);
 });
 
-module.exports = routes
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  });
+
+  return response.json({ id });
+});
+
+module.exports = routes;
