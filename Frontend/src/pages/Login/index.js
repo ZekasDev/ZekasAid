a5c053d3c7ca042eff843f1f711453697e730b7a
ZekasDev <dannndourado@gmail.com>
2023-02-07 12:40:42 -0300

    Adicionando botão 'Recuperar senha'

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./styles.css";
-import { FiLogIn } from 'react-icons/fi' //npm install react-icons assim que chegar em casa
-import frontImg from '../../assets/zekasaid.jpg'
-import logoImg from '../../assets/logo.jpg'
+import { FiLogIn, FiAlertTriangle } from "react-icons/fi";
+import frontImg from "../../assets/zekasaid.jpg";
+import logoImg from "../../assets/logo.jpg";
 
 export default function Login() {
   return (
-    <div className="login-container"> 
-        <section className="form">
-        <img src={logoImg} alt='Logo'/>
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImg} alt="Logo" />
         <form>
           <h1>Faça seu Login</h1>
           <input placeholder="Usuário" />
-          <button className="button" type="submit">Login</button>
+          <button className="button" type="submit">
+            Login
+          </button>
           <a href="/register">
-            <FiLogIn size={16} color="green"/>
+            <FiLogIn size={16} color="#01ab6d" />
             Cadastrar
           </a>
+          <a href="/recover">
+            <FiAlertTriangle size={16} color="#01ab6d" />
+            Esqueci minha senha
+          </a>
         </form>
-        </section>  
-        <img src={frontImg} alt='Background Img'/>
+      </section>
+      <img src={frontImg} alt="Background Img" />
     </div>
-  )
+  );
 }
