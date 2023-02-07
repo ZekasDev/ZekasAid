54f557eeff552314d5e9c1e24ed2d13cc1915df1
ZekasDev <dannndourado@gmail.com>
2023-02-07 08:08:09 -0300

    Tela de Login - Desenvolvimento

--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./styles.css";
 import frontImg from '../../assets/zekasaid.jpg'
+import logoImg from '../../assets/logo.jpg'
 
 export default function Login() {
   return (
     <div className="login-container"> 
         <section className="form">
-        
+        <img src={logoImg} alt='Logo'/>
+        <form>
+          <h1>Faça seu Login</h1>
+          <input placeholder="Usuário" />
+          <button type="submit">Login</button>
+          <a href="/register">
+            Cadastrar
+          </a>
+        </form>
         </section>  
-        <img src={frontImg} />
+        <img src={frontImg} alt='Background Img'/>
     </div>
   )
 }
